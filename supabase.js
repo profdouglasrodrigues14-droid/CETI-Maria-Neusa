a6afe566b9c343b02add68d7d788db2549b9f153
profdouglasrodrigues14-droid <prof.douglasrodrigues14@gmail.com>
2026-06-30 13:56:33 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const supabaseUrl = "https://sbwtvvtyjtzouokugrxb.supabase.co";
 
-const supabaseKey = "COLE_AQUI_SUA_ANON_KEY";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNid3R2dnR5anR6b3Vva3VncnhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NDQ3NDgsImV4cCI6MjA5ODEyMDc0OH0.GbUEMQIylmGhlYcjWynCGmGkQWMhSgsqWBXl8rBzOxU";
 
 export const supabase = createClient(
     supabaseUrl,
